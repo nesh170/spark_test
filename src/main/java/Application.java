ffb1639e8f3a4c0ea6e97e9709c59f9930785cff
Sivaneshwaran Loganathan <sl290@duke.edu>
2016-10-13 13:54:47 -0400

    Testing IntelliJ commit

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -4,6 +4,6 @@ public class Application {
     public static void main(String[] args) {
         port(8080);
         get("/hello", (req, res) -> "Hello World");
-        get("/",((request, response) -> "Testing this website to see if it is lit\n"));
+        get("/",((request, response) -> "Testing this website to see if it is litz\n"));
     }
 }
