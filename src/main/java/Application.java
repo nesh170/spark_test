bf8466ce00a5d0a45a63994c70719bc168fd1887
Sivaneshwaran Loganathan <sivaneshwaran.loganathan@duke.edu>
2016-10-26 20:13:15 -0400

    updated gitignore and test

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,9 +1,20 @@
+import java.sql.Timestamp;
+import java.util.Date;
+
 import static spark.Spark.*;
 
 public class Application {
+    public static int myCounter = 0;
+
     public static void main(String[] args) {
         port(8080);
-        get("/hello", (req, res) -> "Hello World");
-        get("/",((request, response) -> "Testing this website to see if it is litz\n"));
+        get("/hello", (req, res) -> {
+            String returnString;
+            Date currentDate = new Date();
+            Timestamp time = new Timestamp(currentDate.getTime());
+            myCounter++;
+            returnString = "Counter: " + myCounter + " Timestamp: " + time.toString();
+            return returnString;
+        });
     }
 }
